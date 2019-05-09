877f5dcdb751c7cc6584bf752ebcfaea30bc94c8
Yehan Pemarathne <yehancha@gmail.com>
2019-05-09 16:36:59 +0530

    Add React Router

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Still just a sample React PWA where all the initial basic packages are inegrated such as React Router.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
